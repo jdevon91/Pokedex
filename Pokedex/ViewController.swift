893b50c154d51a1daa3bc634b1b154ed578d5bc2
jdevon91 <jbudhlall@gmail.com>
2017-02-06 16:54:55 -0700

    Added audio track, and audio track toggle button.

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
     @IBOutlet weak var collection: UICollectionView!
+    @IBAction func musicBtnPressed(_ sender: UIButton) {
+        
+        if musicPlayer.isPlaying{
+            
+            musicPlayer.pause()
+            sender.alpha = 0.2
+            
+        }
+        else{
+            
+            musicPlayer.play()
+            sender.alpha = 1.0
+            
+        }
+        
+    }
     
     var pokemon = [Pokemon]()
+    var musicPlayer = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +39,27 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         collection.delegate = self
         
         parsePokemonCSV()
+        initAudio()
+    }
+    
+    func initAudio() {
+        
+        let path = Bundle.main.path(forResource: "music", ofType: "mp3")
+        
+        do {
+            
+            musicPlayer = try AVAudioPlayer(contentsOf: URL(string: path!)!)
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1
+            musicPlayer.play()
+            
+        }
+        catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
+            
+        
     }
     //function to parse pokemon.csv
     func parsePokemonCSV() {
@@ -77,7 +116,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     //Set the number of items in the section.
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-        return 30
+        return pokemon.count
         
     }
     
