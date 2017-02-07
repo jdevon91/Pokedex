4fb779b90c7aafc8bcce0d5c846ea05e7f3579cc
jdevon91 <jbudhlall@gmail.com>
2017-02-07 11:16:13 -0700

    Search bar implemented.

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -9,9 +9,10 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
 
     @IBOutlet weak var collection: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBAction func musicBtnPressed(_ sender: UIButton) {
         
         if musicPlayer.isPlaying{
@@ -26,22 +27,27 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             sender.alpha = 1.0
             
         }
-        
     }
     
     var pokemon = [Pokemon]()
+    var filteredPokemon = [Pokemon]()
     var musicPlayer = AVAudioPlayer()
+    var inSearchMode = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collection.dataSource = self
         collection.delegate = self
+        searchBar.delegate = self
+        
+        searchBar.returnKeyType = UIReturnKeyType.done
         
         parsePokemonCSV()
         initAudio()
     }
     
+    //function to initialize audio
     func initAudio() {
         
         let path = Bundle.main.path(forResource: "music", ofType: "mp3")
@@ -57,10 +63,33 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         catch let err as NSError {
             print(err.debugDescription)
         }
-        
-            
-        
     }
+    
+    //function to hide keyboard when return is pressed
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
+    }
+    
+    //function for the search bar
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchBar.text == nil || searchBar.text == ""
+        {
+            inSearchMode = false
+            collection.reloadData()
+            view.endEditing(true)//hide keyboard
+        }
+        else
+        {
+           inSearchMode = true
+        
+        let lower = searchBar.text!.lowercased()
+        
+        filteredPokemon = pokemon.filter({$0.name.range(of: lower) != nil})
+        collection.reloadData()
+        }
+    }
+    
     //function to parse pokemon.csv
     func parsePokemonCSV() {
         
@@ -86,7 +115,6 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         catch let err as NSError {
             print(err.debugDescription)
         }
-        
     }
     
     //deque cells, sets up collection view
@@ -94,8 +122,17 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
             
-            let poke = pokemon[indexPath.row]
-            cell.configureCell(poke)
+            let poke: Pokemon!
+            
+            if inSearchMode {
+                poke = filteredPokemon[indexPath.row]
+                cell.configureCell(poke)
+            }
+            else
+            {
+                poke = pokemon[indexPath.row]
+                cell.configureCell(poke)
+            }
             
             return cell
         }
@@ -104,35 +141,31 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             
             return UICollectionViewCell()
         }
-    
     }
     
     //Action for what happens when the user taps a cell.
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
-        
     }
     
     //Set the number of items in the section.
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
+        if inSearchMode {
+            return filteredPokemon.count
+        }
         return pokemon.count
-        
     }
     
     //Number of sections in the collection view.
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         
         return 1
-        
     }
     
     //Size of cells in collection.
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
         return CGSize(width: 105, height: 105)
-        
     }
-    
 }
 
